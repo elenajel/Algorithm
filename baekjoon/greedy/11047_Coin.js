233725b690ea5c3d9051b8b3e6476a52577d2f3e
Elena Lee <jelee.elena@gmail.com>
2020-07-09 02:44:57 +0900

    Complete 11047_Coin

--- baekjoon/greedy/11047_Coin.js
+++ baekjoon/greedy/11047_Coin.js
@@ -32,7 +32,7 @@ const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  // output: process.stdout,
 });
 
 let input = [];
